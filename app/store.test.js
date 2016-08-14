2d96754285a98612cb0a820ddf135340e9382ad5
Will Ockmore <will.ockmore@gmail.com>
2016-08-14 23:08:38 +0100

    add store

---
+++ app/store.test.js
@@ -0,0 +1,23 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from './store';
+
+describe('store', () => {
+
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      payload: {
+        entries: ['Trainspotting', '28 Days Later']
+      }
+    });
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    }));
+  });
+
+});
