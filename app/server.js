3570790f35ca196bda0321fd8e6b893a59f848fa
Will Ockmore <will.ockmore@gmail.com>
2016-08-15 14:32:23 +0100

    indentation

--- app/server.js
+++ app/server.js
@@ -3,12 +3,12 @@ import Server from 'socket.io';
 export default function startServer() {
   const io = new Server().attach(8090);
 
-store.subscribe(
-  () => io.emit('state', store.getState().toJS())
-);
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
 
-io.on('connection', (socket) => {
-  socket.emit('state', store.getState().toJS());
-});
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
 
 }
