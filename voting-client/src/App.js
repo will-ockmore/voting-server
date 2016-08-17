785c7f37ae4301afb35e8251044340d1ac2efbd0
Will Ockmore <will.ockmore@gmail.com>
2016-08-17 19:47:34 +0100

    basic UI for voting component

--- voting-client/src/App.js
+++ voting-client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>BLAH</h1>
+        <Voting pair={pair} />
       </div>
     );
   }
